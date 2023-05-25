84af64b2d30e3f4af2b5787b7ca92f1b430df199
Dezmerek <mszymanski027@gmail.com>
2023-05-26 00:46:46 +0200

    setting fonts

--- ChatApp/App.js
+++ ChatApp/App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback, useEffect, useState } from "react";
+import * as Font from 'expo-font';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -10,11 +11,34 @@ export default function App() {
   const [appIsLoaded, setAppIsLoaded] = useState(false);
 
   useEffect(() => {
-    //wczytanie fonts
-    setTimeout(() => {
-      setAppIsLoaded(true);
-    }, 2000);
-  });
+
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          "black": require("./assets/fonts//Roboto-Black.ttf"),
+          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
+          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
+          "light": require("./assets/fonts/Roboto-Light.ttf"),
+          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
+          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
+          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
+          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      }
+      catch (error) {
+        console.log.error();
+      }
+      finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
 
   const onLayout = useCallback(async () => {
     if (appIsLoaded) {
