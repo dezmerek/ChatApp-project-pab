5112ca7bbc282585be2a9c70c62719439c9bf8fb
Dezmerek <mszymanski027@gmail.com>
2023-05-26 03:04:36 +0200

    create separate file

--- ChatApp/navigation/AppNavigator.js
+++ ChatApp/navigation/AppNavigator.js
@@ -7,48 +7,15 @@ import { Ionicons } from '@expo/vector-icons';
 import ChatSettingsScreen from '../screens/ChatSettingsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import ChatListScreen from "../screens/ChatListScreen";
+import MainNavigator from "./MainNavigator";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const TabNavigator = () => {
-    return (
-        <Tab.Navigator
-            screenOptions={{ headerTitle: '' }}>
-            <Tab.Screen name="ChatList" component={ChatListScreen} options={{
-                tabBarLabel: 'Czat',
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="chatbubble-outline" size={size} color={color} />
-                )
-            }} />
-            <Tab.Screen name="Settings" component={SettingsScreen} options={{
-                tabBarLabel: 'Ustawienia',
-                tabBarIcon: ({ color, size }) => (
-                    <Ionicons name="settings-outline" size={size} color={color} />
-                )
-            }} />
-        </Tab.Navigator>
-    )
-}
-
 const AppNavigator = (props) => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Home"
-                    component={TabNavigator}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="ChatSettings"
-                    component={ChatSettingsScreen}
-                    options={{
-                        headerTitle: "Ustawienia",
-                        headerBackTitle: "Wróć",
-                    }}
-                />
-            </Stack.Navigator>
+            <MainNavigator />
         </NavigationContainer>
     );
 };
