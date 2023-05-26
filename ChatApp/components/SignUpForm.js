44169c0e331c28633ac32284a905e3ae33e93d9b
Dezmerek <mszymanski027@gmail.com>
2023-05-27 01:02:00 +0200

    add firebase sdk

--- ChatApp/components/SignUpForm.js
+++ ChatApp/components/SignUpForm.js
@@ -7,6 +7,24 @@ import { validateInput } from '../utils/actions/formActions';
 import { reducer } from '../utils/reducers/formReducer';
 import { signUp } from '../utils/actions/authActions';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDqH7N-SJEubNocS8G_Wx1iEubcg0yNrto",
+    authDomain: "chatapp-4255d.firebaseapp.com",
+    projectId: "chatapp-4255d",
+    storageBucket: "chatapp-4255d.appspot.com",
+    messagingSenderId: "667716197293",
+    appId: "1:667716197293:web:bf08d4946c464d7b819f24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const initialState = {
     inputValues: {
         firstName: "",
