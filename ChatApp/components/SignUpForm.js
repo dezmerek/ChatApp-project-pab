fefc1e8da2d0aa3dacf4002c95e9b6e56392ef6c
Dezmerek <mszymanski027@gmail.com>
2023-05-26 23:38:33 +0200

    useReducer

--- ChatApp/components/SignUpForm.js
+++ ChatApp/components/SignUpForm.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Input from '../components/Input';
 import SubmitButton from '../components/SubmitButton';
 import { Feather, FontAwesome } from '@expo/vector-icons';
+
 import { validateInput } from '../utils/actions/formActions';
+
+const reducer = (state, action) => {
+    const { validationResult } = action;
+    return {
+        ...state,
+        formIsValid: validationResult === undefined
+    };
+}
+
+const initialState = {
+    inputValidities: {
+        firstName: false,
+        lastName: false,
+        email: false,
+        password: false,
+    },
+    formIsValid: false
+}
 
 const SignUpForm = props => {
 
+    const [formState, dispatchFormState] = useReducer(reducer, initialState);
+
     const inputChangedHandler = (inputId, inputValue) => {
-        console.log(validateInpu(inputId, inputValue))
+        const result = validateInput(inputId, inputValue);
+        dispatchFormState({ validationResult: result })
     }
 
     return (
@@ -50,7 +72,8 @@ const SignUpForm = props => {
             <SubmitButton
                 title="Utwórz konto"
                 onPress={() => console.log("Button pressed")}
-                style={{ marginTop: 20 }} />
+                style={{ marginTop: 20 }}
+                disabled={!formState.formIsValid} />
         </>
     )
 };
