f77552acb88564ba6a52daf6e0d30a12bb8b91ff
Dezmerek <mszymanski027@gmail.com>
2023-05-29 00:19:12 +0200

    create search box

--- ChatApp/constants/colors.js
+++ ChatApp/constants/colors.js
@@ -1,6 +1,7 @@
 export default {
     blue: "#3498db",
     lightGrey: "#bdc3c7",
+    extraLightGrey: "#ededed",
     nearlyWhite: "#F4F8F7",
     grey: "#7f8c8d",
     textColor: "#1c1e21",
--- ChatApp/screens/NewChatScreen.js
+++ ChatApp/screens/NewChatScreen.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import PageContainer from '../components/PageContainer';
+import { FontAwesome } from '@expo/vector-icons';
+import colors from '../constants/colors';
 
 const NewChatScreen = props => {
 
@@ -11,23 +14,41 @@ const NewChatScreen = props => {
                 return <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                     <Item
                         title="Zamknij"
-                        onPress={() => props.navigation.goback()} />
+                        onPress={() => props.navigation.goBack()} />
                 </HeaderButtons>
             },
             headerTitle: "Nowy czat"
         })
     }, []);
 
-    return <View style={styles.container}>
-        <Text>Nowy czat</Text>
-    </View>
+    return <PageContainer>
+        <View style={styles.searchContainer}>
+            <FontAwesome name="search" size={15} color={colors.lightGrey} />
+
+            <TextInput
+                placeholder='Szukaj'
+                style={styles.searchBox}
+                onChangeText={() => { }}
+            />
+        </View>
+    </PageContainer>
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    searchContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: colors.extraLightGrey,
+        height: 30,
+        marginVertical: 8,
+        paddingHorizontal: 8,
+        paddingVertical: 5,
+        borderRadius: 5
+    },
+    searchBox: {
+        marginLeft: 8,
+        fontSize: 15,
+        width: '100%'
     }
 })
 
