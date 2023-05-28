c947d9e986206159d0bf66ad1bb82d5c5ec37b73
Dezmerek <mszymanski027@gmail.com>
2023-05-28 20:38:15 +0200

    open image picker

--- ChatApp/utils/imagePickerHelper.js
+++ ChatApp/utils/imagePickerHelper.js
@@ -4,6 +4,16 @@ import { Platform } from 'react-native';
 export const launchImagePicker = async () => {
     await checkMediaPermissions();
 
+    const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1
+    });
+
+    if (!result.cancelled) {
+        console.log(result.uri);
+    }
 }
 
 const checkMediaPermissions = async () => {
