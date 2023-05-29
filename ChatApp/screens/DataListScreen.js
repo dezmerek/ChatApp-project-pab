ad66faec8104e318ceb59d1eb8f577d9a88991f3
Dezmerek <mszymanski027@gmail.com>
2023-05-29 16:24:41 +0200

    all users

--- ChatApp/screens/DataListScreen.js
+++ ChatApp/screens/DataListScreen.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native';
+import { FlatList, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import DataItem from '../components/DataItem';
+import PageContainer from '../components/PageContainer';
 
 const DataListScreen = props => {
+
+    const storedUsers = useSelector(state => state.users.storedUsers);
 
     const { title, data, type, chatId } = props.route.params;
 
@@ -9,7 +14,38 @@ const DataListScreen = props => {
         props.navigation.setOptions({ headerTitle: title })
     }, [title])
 
-    return <Text>This is the data list</Text>
+    return <PageContainer>
+        <FlatList
+            data={data}
+            keyExtractor={item => item}
+            renderItem={(itemData) => {
+                let key, onPress, image, title, subTitle, itemType;
+
+                if (type === "users") {
+                    const uid = itemData.item;
+                    const currentUser = storedUsers[uid];
+
+                    if (!currentUser) return;
+
+                    key = uid;
+                    image = currentUser.profilePicture;
+                    title = `${currentUser.firstName} ${currentUser.lastName}`;
+                    subTitle = currentUser.about;
+                    itemType = "link";
+                }
+
+                return <DataItem
+                    key={key}
+                    onPress={onPress}
+                    image={image}
+                    title={title}
+                    subTitle={subTitle}
+                    type={itemType}
+                />
+            }}
+        />
+
+    </PageContainer>
 };
 
 export default DataListScreen;
