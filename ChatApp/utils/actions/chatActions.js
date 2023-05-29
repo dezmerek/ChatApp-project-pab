729a8a9ea153e76b5ab8348de9ee60c98f781935
Dezmerek <mszymanski027@gmail.com>
2023-05-29 16:54:18 +0200

    send push

--- ChatApp/utils/actions/chatActions.js
+++ ChatApp/utils/actions/chatActions.js
@@ -1,5 +1,6 @@
 import { child, get, getDatabase, push, ref, remove, set, update } from "firebase/database";
 import { getFirebaseApp } from "../firebaseHelper";
+import { getUserPushTokens } from "./authActions";
 import { addUserChat, deleteUserChat, getUserChats } from "./userActions";
 
 export const createChat = async (loggedInUserId, chatData) => {
@@ -25,8 +26,12 @@ export const createChat = async (loggedInUserId, chatData) => {
     return newChat.key;
 }
 
-export const sendTextMessage = async (chatId, senderId, messageText, replyTo) => {
-    await sendMessage(chatId, senderId, messageText, null, replyTo, null);
+export const sendTextMessage = async (chatId, senderData, messageText, replyTo, chatUsers) => {
+    console.log("ABC");
+    await sendMessage(chatId, senderData.userId, messageText, null, replyTo, null);
+
+    const otherUsers = chatUsers.filter(uid => uid !== senderData.userId);
+    await sendPushNotificationForUsers(otherUsers, `${senderData.firstName} ${senderData.lastName}`, messageText);
 }
 
 export const sendInfoMessage = async (chatId, senderId, messageText) => {
@@ -160,4 +165,27 @@ export const addUsersToChat = async (userLoggedInData, usersToAddData, chatData)
     const messageText = `${userLoggedInData.firstName} ${userLoggedInData.lastName} added ${userAddedName} ${moreUsersMessage}to the chat`;
     await sendInfoMessage(chatData.key, userLoggedInData.userId, messageText);
 
+}
+
+const sendPushNotificationForUsers = (chatUsers, title, body) => {
+    chatUsers.forEach(async uid => {
+        console.log("test");
+        const tokens = await getUserPushTokens(uid);
+
+        for (const key in tokens) {
+            const token = tokens[key];
+
+            await fetch("https://exp.host/--/api/v2/push/send", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    to: token,
+                    title,
+                    body
+                })
+            })
+        }
+    })
 }
