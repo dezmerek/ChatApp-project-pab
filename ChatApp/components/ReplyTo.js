e0f2127920109973a0eae814fdd75d5c63a730fc
Dezmerek <mszymanski027@gmail.com>
2023-05-29 10:52:25 +0200

    areply to style

--- ChatApp/components/ReplyTo.js
+++ ChatApp/components/ReplyTo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from '../constants/colors';
 
 const ReplyTo = props => {
     const { text, user, onCancel } = props;
@@ -16,7 +17,23 @@ const ReplyTo = props => {
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: colors.extraLightGrey,
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderLeftColor: colors.blue,
+        borderLeftWidth: 4
+    },
+    textContainer: {
+        flex: 1,
+        marginRight: 5
+    },
+    name: {
+        color: colors.blue,
+        fontFamily: 'medium',
+        letterSpacing: 0.3
+    }
 });
 
 export default ReplyTo;
