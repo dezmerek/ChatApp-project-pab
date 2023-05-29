c867cab6e4962fdf472235d8fbaa7e543f4cd4b0
Dezmerek <mszymanski027@gmail.com>
2023-05-29 16:06:24 +0200

    add

--- ChatApp/components/DataItem.js
+++ ChatApp/components/DataItem.js
@@ -2,35 +2,50 @@ import React from 'react';
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import colors from '../constants/colors';
 import ProfileImage from './ProfileImage';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+
+const imageSize = 40;
 
 const DataItem = props => {
 
-    const { title, subTitle, image, type, isChecked } = props;
+    const { title, subTitle, image, type, isChecked, icon } = props;
 
     return (
         <TouchableWithoutFeedback onPress={props.onPress}>
             <View style={styles.container}>
 
-                <ProfileImage
-                    uri={image}
-                    size={40}
-                />
+                {
+                    !icon &&
+                    <ProfileImage
+                        uri={image}
+                        size={imageSize}
+                    />
+                }
+
+                {
+                    icon &&
+                    <View style={styles.leftIconContainer}>
+                        <AntDesign name={icon} size={20} color={colors.primary} />
+                    </View>
+                }
 
 
                 <View style={styles.textContainer}>
 
                     <Text
                         numberOfLines={1}
-                        style={styles.title}>
+                        style={{ ...styles.title, ...{ color: type === "button" ? colors.blue : colors.textColor } }}>
                         {title}
                     </Text>
 
-                    <Text
-                        numberOfLines={1}
-                        style={styles.subTitle}>
-                        {subTitle}
-                    </Text>
+                    {
+                        subTitle &&
+                        <Text
+                            numberOfLines={1}
+                            style={styles.subTitle}>
+                            {subTitle}
+                        </Text>
+                    }
 
                 </View>
 
@@ -86,6 +101,14 @@ const styles = StyleSheet.create({
     checkedStyle: {
         backgroundColor: colors.primary,
         borderColor: 'transparent'
+    },
+    leftIconContainer: {
+        backgroundColor: colors.extraLightGrey,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: imageSize,
+        height: imageSize
     }
 });
 
