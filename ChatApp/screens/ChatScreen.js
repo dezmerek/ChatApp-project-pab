bd3d3c42251b8c64c4b34f39dcb158f67f89c2c0
k-wos <kacper.wos33@gmail.com>
2023-05-29 18:41:18 +0200

    fixed textbox

--- ChatApp/screens/ChatScreen.js
+++ ChatApp/screens/ChatScreen.js
@@ -326,7 +326,7 @@ const styles = StyleSheet.create({
         flex: 1,
         borderWidth: 1,
         borderRadius: 50,
-        borderColor: colors.red,
+        borderColor: colors.grey,
         marginHorizontal: 15,
         paddingHorizontal: 12,
     },
